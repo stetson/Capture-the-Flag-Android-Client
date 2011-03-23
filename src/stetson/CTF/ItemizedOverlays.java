0680d7e4e90bbf76f437426dbfccfcd741942b01
jeremyy <jyanik@stetson.edu>
2011-03-23 16:50:43 -0400

    Added MapView to layout. Setup itemized overlays. Commented a bunch of stuff.

---
+++ src/stetson/CTF/ItemizedOverlays.java
@@ -0,0 +1,73 @@
+package stetson.CTF;
+
+import java.util.ArrayList;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class ItemizedOverlays extends ItemizedOverlay {
+
+	
+	private Context mContext;
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	
+	/**
+	 * 
+	 * @param defaultMarker
+	 */
+	public ItemizedOverlays(Drawable defaultMarker) {
+		  super(boundCenterBottom(defaultMarker));
+	}
+	
+	/**
+	 * 
+	 * @param defaultMarker
+	 * @param context
+	 */
+	public ItemizedOverlays(Drawable defaultMarker, Context context) {
+		  super(defaultMarker);
+		  mContext = context;
+	}
+	
+	/**
+	 * Adds an overlay item to the array of overlays.
+	 * @param overlay
+	 */
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+	
+	/**
+	 * Displays an alert dialog with the title and snippet of an overlay item.
+	 * @param index of an overlay item
+	 * @return true
+	 */
+	protected boolean onTap(int index) {
+	  OverlayItem item = mOverlays.get(index);
+	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+	  dialog.setTitle(item.getTitle());
+	  dialog.setMessage(item.getSnippet());
+	  dialog.show();
+	  return true;
+	}
+	
+	/**
+	 * 
+	 * @param index of an overlay item
+	 * @return the specified overlay item
+	 */
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
+	}
+
+	/**
+	 * @return number of overlays in array
+	 */
+	public int size() {
+		return mOverlays.size();
+	}
+
+}
--- src/stetson/CTF/StetsonCTF.java
+++ src/stetson/CTF/StetsonCTF.java
@@ -1,21 +1,47 @@
 package stetson.CTF;
 
-import android.app.Activity;
+import java.util.List;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class StetsonCTF extends MapActivity {
 	
-    /** Called when the activity is first created. */
-    @Override
+	/**
+	  * Runs when the activity is started (by Android)
+	  * @param saved instance state
+	  */
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Turns on built-in zoom controls
+        MapView mapView = (MapView) findViewById(R.id.mapView);
+        mapView.setBuiltInZoomControls(true);
+        
+        // Setting up the overlays class
+        List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.person_red);
+        ItemizedOverlays itemizedoverlay = new ItemizedOverlays(drawable);
+        
+        // Adding a marker to the map
+        GeoPoint point = new GeoPoint(19240000,-99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
     }
 
-	@Override
+	/**
+	 * Returns false (required by MapActivity)
+	 * @return false
+	 */
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
+	
 }
